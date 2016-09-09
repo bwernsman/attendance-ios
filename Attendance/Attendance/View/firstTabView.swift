3bcf4fff4801ee3ae8fd01cf21f5b9d8a3254908
Ben Wernsman <benwernsman@Bens-MacBook-Pro.local>
2016-09-09 14:19:46 -0500

    Added WebViews
    -Added web views inside the 3 tab views (all going to different
    websites)
    -Added tab names

--- Attendance/Attendance/View/firstTabView.swift
+++ Attendance/Attendance/View/firstTabView.swift
@@ -9,17 +9,36 @@
 import Foundation
 import UIKit
 
-class firstTabView: UIViewController {
+class firstTabView: UIViewController, UIWebViewDelegate {
+    
+    @IBOutlet weak var webView: UIWebView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //Set title
+        self.title = "Google"
+        
+        //Load Website
+        let url = NSURL (string:"https://www.google.com");
+        let requestObj = NSURLRequest(URL: url!);
+        webView.delegate = self
+        webView.loadRequest(requestObj);
+    }
+    
+    func webViewDidStartLoad(webView: UIWebView) {
+        print("Did start loading")
+        
+    }
+    
+    func webViewDidFinishLoad(webView : UIWebView) {
+        print("Ended loading")
+        //indicator.stopAnimating()
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    
 }
--- Attendance/Attendance/View/secondTabView.swift
+++ Attendance/Attendance/View/secondTabView.swift
@@ -9,18 +9,36 @@
 import Foundation
 import UIKit
 
-class secondTabView: UIViewController {
+class secondTabView: UIViewController, UIWebViewDelegate {
+    
+    @IBOutlet weak var webView: UIWebView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        //Set title
+        self.title = "Apple"
+        
+        //Load Website
+        let url = NSURL (string:"https://www.apple.com");
+        let requestObj = NSURLRequest(URL: url!);
+        webView.delegate = self
+        webView.loadRequest(requestObj);
+    }
+    
+    func webViewDidStartLoad(webView: UIWebView) {
+        print("Did start loading")
+        
+    }
+    
+    func webViewDidFinishLoad(webView : UIWebView) {
+        print("Ended loading")
+        //indicator.stopAnimating()
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    
 }
-
