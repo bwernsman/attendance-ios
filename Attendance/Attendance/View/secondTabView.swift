801b97fa6a79659fe1fc79629b571087e3528e85
Ben Wernsman <benwernsman@Bens-MacBook-Pro.local>
2016-09-09 14:25:51 -0500

    Activity Indicator
    -Added a spinner for Apple (tab 2), I like hiding the page until it is
    fully loaded

--- Attendance/Attendance/View/secondTabView.swift
+++ Attendance/Attendance/View/secondTabView.swift
@@ -12,6 +12,7 @@ import UIKit
 class secondTabView: UIViewController, UIWebViewDelegate {
     
     @IBOutlet weak var webView: UIWebView!
+    @IBOutlet weak var indicator: UIActivityIndicatorView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,16 +26,21 @@ class secondTabView: UIViewController, UIWebViewDelegate {
         let requestObj = NSURLRequest(URL: url!);
         webView.delegate = self
         webView.loadRequest(requestObj);
+        
+        indicator.startAnimating()
+        indicator.hidesWhenStopped = true
     }
     
     func webViewDidStartLoad(webView: UIWebView) {
         print("Did start loading")
+        webView.alpha = 0.0
         
     }
     
     func webViewDidFinishLoad(webView : UIWebView) {
         print("Ended loading")
-        //indicator.stopAnimating()
+        indicator.stopAnimating()
+        webView.alpha = 1.0
     }
     
     override func didReceiveMemoryWarning() {
